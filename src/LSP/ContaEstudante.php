aa6436960a1942af7d90b70feb9216d3da5900ba
Alex Moreno <alex.moreno.costa@gmail.com>
2017-05-16 21:56:36 -0300

    Criado base para o exercicio

---
+++ src/LSP/ContaEstudante.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Amc\Solid\LSP;
+
+class ContaEstudante extends ContaComum
+{
+    private $milhas;
+
+    public function deposita($valor)
+    {
+        parent::deposita($valor);
+        $this->milhas += $valor;
+    }
+
+    public function getMilhas()
+    {
+        return $this->milhas;
+    }
+
+    public function rende()
+    {
+        throw new \Exception("Conta de estudante não rende");
+    }
+}
